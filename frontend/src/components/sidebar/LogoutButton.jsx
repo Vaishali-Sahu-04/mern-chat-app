45bc49e6b76e7a48883b5fe524ac1dbd299e86e2
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-03-20 19:11:06 +0530

    2nd part completed - UI design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import { BiLogOut } from "react-icons/bi";
+//import useLogout from "../../hooks/useLogout";
+
+const LogoutButton = () => {
+	//const { loading, logout } = useLogout();
+
+	return (
+		<div className='mt-auto'>
+			
+		</div>
+	);
+};
+export default LogoutButton;
